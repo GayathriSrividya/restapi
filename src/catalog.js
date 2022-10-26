b5bb667b6300f4888869cbd767f78f70c06e0f74
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-10-26 18:11:06 +0530

    script for product catalog api

--- src/catalog.js
+++ src/catalog.js
@@ -3,27 +3,22 @@ const app=express()
 app.use(express.json())
 const readData=require('./fileHandling.js')
 
-
 app.get('/', (req, res) => {
 res.send('hello, welcome to our product catalog.')
 })
-
 
 app.get('/products', (req, res)=>{
     res.send(readData.fromFile(req, res))
 })
 
-
 app.get('/products/:id', (req, res)=>{
      res.send(readData.filter(req, res))
 })
-
 
 app.post('/products/add', (req, res)=>{
     
      res.send(readData.toFile(req, res))
     })
-
 
 app.listen(8080, ()=>
 {
